17ec848a71ac305fd9c8a7d9cb4686f435ce94fc
Данил <Ovdan1992@mail.ru>
2020-12-23 17:48:48 +0300

    Добавление случайного отображения контактов

--- ContactAPP/ViewController/ContactListViewControllerTableViewController.swift
+++ ContactAPP/ViewController/ContactListViewControllerTableViewController.swift
@@ -18,7 +18,7 @@ class ContactListViewControllerTableViewController: UITableViewController {
     // MARK: - Table view data source
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+        
         return contactList.count 
     }
 
@@ -26,31 +26,16 @@ class ContactListViewControllerTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
 
-        // Configure the cell...
+        var ferstContact = contactList[indexPath.row]
+        cell.textLabel?.text = ferstContact.name + " " +  ferstContact.secondName
 
         return cell
     }
     
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
+   
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
